2f8c1e1203d61d0a96b6efdd867ac96f4f12833b
NicV2 <nicv2code@gmail.com>
2017-04-07 18:50:15 -0700

    For loop changed to starting at zero and num++)

--- problem1.js
+++ problem1.js
@@ -1,10 +1,10 @@
 //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 //Find the sum of all the multiples of 3 or 5 below 1000.
 
-var num = 999;
+var num = 0;
 var sumMultiples = 0;
 
-for(num = 999; num>0; num--)
+for(num = 0; num<1000; num++)
 {
 
   if (num % 3 === 0 || num % 5 === 0)
